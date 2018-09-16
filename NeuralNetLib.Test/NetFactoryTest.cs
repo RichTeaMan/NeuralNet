9789e95c1b55ae562a2d9230f92c04f8c5a14671
Thomas Holmes <thomasholmes@outlook.com>
2018-09-16 13:47:12 +0100

    Added obsolete warning supression in unit test.

--- NeuralNetLib.Test/NetFactoryTest.cs
+++ NeuralNetLib.Test/NetFactoryTest.cs
@@ -25,7 +25,9 @@ namespace RichTea.NeuralNetLib.Test
             Random random = new Random(5);
             Net net = netFactory.GenerateRandomNet(3, 1, random);
 
+#pragma warning disable CS0618 // Type or member is obsolete
             Net mutatednet = netFactory.CreateMutatedNet(net, random, 0.001);
+#pragma warning restore CS0618 // Type or member is obsolete
 
             Assert.AreNotEqual(net.CreateSerialisedNet(), mutatednet.CreateSerialisedNet());
 
